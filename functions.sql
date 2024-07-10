153e1df0cd2d00e12210fe4a26c58175678c527f
Daniel Van Den Bosch <danielleevandenbosch@gmail.com>
2024-07-10 08:30:09 -0400

    Update functions.sql

--- functions.sql
+++ functions.sql
@@ -13,6 +13,11 @@ $$ LANGUAGE plpgsql;
 
 CREATE OR REPLACE FUNCTION AAND(BOOLEAN, BOOLEAN)
 RETURNS BOOLEAN AS $$
+/*
+    Author: Daniel Van Den Bosch
+    Date: 2024-07-10
+    Telos: The purpose of this function is to add backward compatibility for ms-excel.
+*/
 BEGIN
     RETURN $1 AND $2;
 END;
